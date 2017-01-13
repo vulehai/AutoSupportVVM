63e628eb9588097ff75e0b08691bb1b8d525069c
Hai.Vu_Git <vulehai.aptech@gmail.com>
2017-01-13 10:08:46 +0700

    Commit before setup new Window

---
+++ AutoChanges/TabGetCommonResource.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace AutoChanges
+{
+    /// <summary>
+    /// Interaction logic for TabGetCommonResource.xaml
+    /// </summary>
+    public partial class TabGetCommonResource : UserControl
+    {
+        public TabGetCommonResource()
+        {
+            InitializeComponent();
+        }
+        public void getId(string filename)
+        {
+            bool check = false;
+           
+            StreamReader sr = new StreamReader(filename);
+            string rows = sr.ReadToEnd();
+            sr.Close();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(rows);
+            String[] Value_List = Regex.Split(txtValue.Text.Trim(), "\r\n");
+            for (int i = 0; i < Value_List.Length; i++)
+            {
+                bool check_value = false;
+                string xId = "/root/data/value";
+                XmlNodeList root = doc.SelectNodes(xId);
+                if (root != null)
+                {
+                    foreach (XmlNode x in root)
+                    {
+                        if (x.InnerText == Value_List[i].Trim())
+                        {
+                            check_value = true;
+                            XmlElement data = (XmlElement)x.ParentNode;
+                           
+                                txtID.Text += data.GetAttribute("name") + "\r\n";
+                               
+                            
+                            break;
+                        }
+                    }
+                    
+                }
+                if(check_value == true)
+                {
+                    txtNewValue.Text += "\r\n";
+                }
+                else
+                {
+                    txtNewValue.Text += Value_List[i].ToString() + "\r\n";
+                    txtID.Text += "\r\n";
+                }
+            }
+            //if (check == true)
+            //{
+            //    StreamWriter sw = new StreamWriter(filename);
+            //    sw.WriteLine(doc.InnerXml);
+
+            //    sw.Close();
+            //    sw.Dispose();
+            //}
+
+        }
+        private void btnGetID_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileNameTextBox.Text == "") {
+                MessageBox.Show("Chọn folder kìa chế ơi!");
+                return;
+            }
+            txtID.Text = "";
+            txtNewValue.Text = "";
+            string dinh_dang_file = "*.resx";
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = FileNameTextBox.Text;
+            StringBuilder sb = new StringBuilder();
+
+            String[] allfiles = System.IO.Directory.GetFiles(path, dinh_dang_file, System.IO.SearchOption.AllDirectories);
+            foreach (string txtName in System.IO.Directory.GetFiles(path, dinh_dang_file, System.IO.SearchOption.AllDirectories))
+            {
+                string path_file = "";
+                if (LBL_NAME.IsChecked == true)
+                {
+                    path_file = path + "\\LBL.resx";
+                }else{
+                    path_file = path + "\\BTN.resx";
+                }
+                //replaceString(txtName);
+                if (txtName == path_file)
+                {
+                    getId(path_file);
+
+                }
+            }
+            MessageBox.Show("Delete done!");
+        }
+      
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+               // FileNameTextBox.Text = result.ToString();
+                FileNameTextBox.Text = dialog.SelectedPath;
+            }
+           
+            //Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            //// Set filter for file extension and default file extension
+            ////dlg.DefaultExt = ".txt";
+            ////dlg.Filter = "Text documents (.txt)|*.txt";
+            //dlg.Filter = "Test *.*|*.*";
+          
+            //// Display OpenFileDialog by calling ShowDialog method
+            //Nullable<bool> result = dlg.ShowDialog();
+
+            //// Get the selected file name and display in a TextBox
+            //if (result == true)
+            //{
+            //    // Open document
+            //    string filename = dlg.FileName;
+            //    FileNameTextBox.Text = filename;
+            //}
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void BTN_NAME_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AutoChanges/tabGridCreater.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoChanges
+{
+    /// <summary>
+    /// Interaction logic for tabGridCreater.xaml
+    /// </summary>
+    public partial class tabGridCreater : UserControl
+    {
+        string resultInitCol = "";
+        string functionGridInitStart = "private void metInitializeGrid()\n{\n\ttry\n\t{";
+        string functionGridInitEnd = "\n\t}\n\tcatch (Exception ex)\n\t{\n\t\tthrow ex;\n\t}\n}";
+        string functionCreateTableStart = " private DataTable metTableSchemaDefineGrid()\n{\n\tDataTable tDt = null;\n\ttry\n\t{\n\t\tDictionary<string, Type> tDicUser = new Dictionary<string, Type>();\n";
+        string functionCreateTableEnd = "\n\t}\n\tcatch (Exception ex)\n\t{\n\t\tthrow ex;\n\t}\n}";
+        string functionCreateDataStart = "private void metGetGridData()\n{\n\tthis.eGridDataSource = metTableSchemaDefineGrid();";
+        string functionCreateDataEnd = "\n\tthis.eGridHelper.BindItemSource(this.eGridDataSource);\n}";
+        public tabGridCreater()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, RoutedEventArgs e)
+        {
+            string[] headerNm = txtHeader.Text.Trim().Split(new[] { "\r\n" }, StringSplitOptions.None);
+            string[] resNm = txtResource.Text.Trim().Split(new[] { "\r\n" }, StringSplitOptions.None);
+            string[] tpNm = txtColType.Text.Trim().Split(new[] { "\r\n" }, StringSplitOptions.None);
+            string[] colMg = txtColMargin.Text.Trim().Split(new[] { "\r\n" }, StringSplitOptions.None);
+            string col = "";
+            string table = "";
+            string data = "";
+            string dataRow = "\n\t\tthis.eGridDataSource.Rows.Add(new object[] { ";
+
+            string resultInitCol = "\t\t  this.eGridHelper = new GridHelper(grd, tbv, true); \n\t\t Column tColumn; \n\t\t";
+
+            if (((headerNm.Length + resNm.Length) - (tpNm.Length + colMg.Length)) != 0)
+            {
+                MessageBoxResult result = MessageBox.Show("Nhầm rùi", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MessageBoxResult result1 = MessageBox.Show("Coi lại đê", "Confirmation", MessageBoxButton.OK, MessageBoxImage.Question);
+                }
+                return;
+            }
+            col += "this.eGridHelper = new GridHelper(grd, view, true, true, true);";
+            col += "\n\t\tColumn tColumn = null;";
+              
+            for (int i = 0; i < headerNm.Length; i++ )
+            {
+                col += "\n\n\t\ttColumn = new Column(\"" + headerNm[i].ToUpper().Replace(" ", "").Replace(".", "") + "\",LBL." + resNm[i] + ");\\\\" + headerNm[i].Trim() ;
+                col += "\n\t\ttColumn.Width = 120;";
+                col += "\n\t\ttColumn.Visible = true;\n\t\ttColumn.AllowEditing = DevExpress.Utils.DefaultBoolean.True;";
+
+                col += "\n\t\ttColumn.HorizontalContentAlignment = DevExpress.Xpf.Editors.Settings.EditSettingsHorizontalAlignment."+UppercaseFirst(colMg[i].ToLower())+";";
+                col += "\n\t\teGridHelper.Add(tColumn);";
+                table += "\n\t\ttDicUser.Add(\""+ headerNm[i].ToUpper().Replace(" ", "").Replace(".", "")+"\", typeof(string));";
+                
+                dataRow += "\""+headerNm[i]+"\",";
+            }
+            col += "\n\t\tthis.eGridHelper.Initialize();";
+            table += "\n\t\treturn tDt = TableUtil.ConvertDictionaryToTable(tDicUser, false, false);";
+            dataRow += "});";
+
+            for (int i = 0; i < 3; i ++ )
+            {
+                data += dataRow;
+            }
+            this.txtResult.Text = functionGridInitStart + col + functionGridInitEnd;
+            this.txtResult.Text += "\n" + functionCreateTableStart + table + functionCreateTableEnd;
+            this.txtResult.Text += "\n" + functionCreateDataStart+data + functionCreateDataEnd;
+        }
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            this.resultInitCol = "";
+            this.txtHeader.Text = "";
+            this.txtResource.Text = "";
+            this.txtColType.Text = "";
+            this.txtColMargin.Text = "";
+            this.txtResult.Text = "";
+        }
+    }
+}
